32aed43886411ffbf35679b375ec9248c507cef5
Ayush K M <kmayushkm@gmail.com>
2022-03-28 19:34:18 +0530

    Made some indemtation changes for increasing the readabilty of the code

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,40 @@
 #include "misc.h"
 
 
-int main()
+int
+main ()
 {
-	while (true)
+  while (true)
+    {
+      clsrc ();
+      PRINT << "Bill Generator" << NEWLINE << NEWLINE;
+      PRINT << "1. Generate a Bill" << NEWLINE;
+      PRINT << "2. Open a Bill" << NEWLINE;
+      PRINT << "3. Add and edit items" << NEWLINE;
+      PRINT << "4. exit" << NEWLINE;
+
+      STRING choice;
+      PRINT << ":>> ";
+      INPUT >> choice;
+
+      if (choice == "1")
 	{
-	clsrc();
-	PRINT << "Bill Generator" << NEWLINE << NEWLINE;
-	PRINT << "1. Generate a Bill" << NEWLINE;
-	PRINT << "2. Open a Bill" << NEWLINE;
-	PRINT << "3. Add and edit items" << NEWLINE;
-	PRINT << "4. exit" << NEWLINE;
-
-	STRING choice;
-	PRINT << ":>> ";
-	INPUT >> choice;
-
-	if(choice == "1"){}
-	else if(choice == "2"){}
-	else if (choice == "3"){}
-	else if (choice == "4"){exit(0);}
-	else{
-		PRINT << "That was inappropriate choice" << NEWLINE;
-		PRINT << "Press Entre";
-		clsrc();
 	}
+      else if (choice == "2")
+	{
 	}
+      else if (choice == "3")
+	{
+	}
+      else if (choice == "4")
+	{
+	  exit (0);
+	}
+      else
+	{
+	  PRINT << "That was inappropriate choice" << NEWLINE;
+	  PRINT << "Press Entre";
+	  clsrc ();
+	}
+    }
 }
