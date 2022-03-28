b8cdc517105cae97dff01c575b3324536f137ddb
Ayush K M <kmayushkm@gmail.com>
2022-03-28 19:31:00 +0530

    Macros for writing the code faster and to increase the readability

---
+++ include/macros.h
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+#ifndef MACROS_HEADER
+#define MACROS_HEADER
+
+//I think using would reduce the time required for writing the code
+#define STRING std::string
+#define PRINT std::cout
+#define INPUT std::cin
+#define NEWLINE std::endl
+
+#endif
